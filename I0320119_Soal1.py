295b53d3235567e515b2525ba0f394ec5b1ab95b
Aji Manarul Aziz <ajimanarul@gmail.com>
2021-03-12 02:48:16 +0700

    pertama buat, mari istirahat dulu

---
+++ I0320119_Soal1.py
@@ -0,0 +1,33 @@
+# Luas Persegi Panjang
+print("===== Luas Persegi Panjang =====")
+length = float(input("Input length: "))
+width = float(input("Input width: "))
+rectangle_area = length * width
+print("Circle area is, rectangle_area)
+
+print("\n")
+
+# Luas Lingkaran
+print("===== Luas Lingkaran =====")
+radius = float(input("Input radius: "))
+pi = 22 / 7
+circle_area = pi * (radius ** 2)
+print("Circle area is", circle_area )
+
+print("\n")
+
+# Luas Kubus
+print("===== Luas Kubus =====")
+edge = float(input("Input edge: "))
+cube_area = 6 * (edge ** 2)
+print("Cube surface area is", cube_area)
+
+# Konversi suhu Celcius ke Fahrenheit
+print("===== Celcius ke Fahrenheit =====")
+celcius = float(input('Input temperature = '))
+print("One day, ")
+
+fahrenheit = ((9/5) * celcius) + 32
+print("maka temperatur fahrenheit adalah", fahrenheit, "fahrenheit")
+
+# Konversi 
