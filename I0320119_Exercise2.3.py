e7a4b1cad3706671bb7082a8f7498f77575eb2a3
Aji Manarul Aziz <80144697+manarulaziz@users.noreply.github.com>
2021-03-12 08:55:12 +0700

    edit phi 3.14 with 22 / 7

--- I0320119_Exercise2.3.py
+++ I0320119_Exercise2.3.py
@@ -7,10 +7,10 @@ print("Luas dan keliling lingkaran")
 r = float(input("Masukkan nilai jari-jari: "))
 
 # Menghitung luas lingkaran
-luas_lingkaran = 3.14 * (r ** 2)
+luas_lingkaran = 22 / 7 * (r ** 2)
 
 # Menghitung keliling lingkaran
-keliling_lingkaran = 2 * 3.14 * r
+keliling_lingkaran = 2 * 22 / 7 * r
 
 # Menampilkan hasil perhitungan ke layar
 print("luas lingkaran: ", luas_lingkaran)
