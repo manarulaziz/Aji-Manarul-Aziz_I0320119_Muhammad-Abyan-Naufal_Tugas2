4c558842d7199fb9067ebed3adc8b87b25fa2d3f
Aji Manarul Aziz <80144697+manarulaziz@users.noreply.github.com>
2021-03-12 08:33:41 +0700

    fixed error

--- I0320119_Exercise2.4.py
+++ I0320119_Exercise2.4.py
@@ -3,10 +3,10 @@
 print("Konversi suhu (Fahrenheit ke Celcius)")
 
 # Input suhu dalam Fahrenheit
-F = float(input("Masukkan suhu(Fahrenheit): ")
+F = float(input("Masukkan suhu(Fahrenheit): "))
 
 # Melakukan konversi suhu ke Celcius
 C = 5 * (F - 32) / 9
 # Menampilkan hasil konversi ke layar
 print("Fahrenheit: ", F)
-print("Celcius", (C))
+print("Celcius", C)
